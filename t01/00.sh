c5cfb2fd1c7c5b2c23e2026acd00256e71e902c6
williamobiana <williamobiana@gmail.com>
2022-11-09 06:14:22 +0000

    dockerfile

--- t01/00.sh
+++ t01/00.sh
@@ -1,2 +1,2 @@
-#delete running container and confirm it is not running
+#stop running container, delete running container and confirm it is not running
 docker ps -q --filter "name=testapp" | grep -q . && docker stop testapp && docker rm -fv testapp
